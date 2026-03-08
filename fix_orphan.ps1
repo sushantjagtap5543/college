9a54a742a117b874180facabfe8439f4a20a8753
sushantjagtap5543 <sushantjagtap.naandi@gmail.com>
2026-03-08 21:42:13 +0530

    chore: remove deleted files from repository

--- fix_orphan.ps1
+++
@@ -1,6 +0,0 @@
-$f = 'c:\Users\HP\Desktop\github\college\frontend\src\App.jsx'
-$lines = [System.IO.File]::ReadAllLines($f)
-# Keep lines 0-451 (1-indexed: 1-452) and 623 onwards (1-indexed: 624+)
-$keep = $lines[0..451] + $lines[623..($lines.Length - 1)]
-[System.IO.File]::WriteAllLines($f, $keep, [System.Text.UTF8Encoding]::new($false))
-Write-Host "Done. Removed lines 453-623. New length: $($keep.Length)"
